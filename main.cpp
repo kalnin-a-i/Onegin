ea17db9624d597795bc89857666f7b699f0bc71d
kalnin-a-i <kalinin.ai@phystech.edu>
2021-09-23 14:20:09 +0300

    added reverse cmp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ void qSort(void *v[], int left, int right, int (*comp)(void *, void *));
 void WriteText(char* lineptrs[], int nlines);
 void FreeMemory(char* lineptrs[], int nlines);
 int RevStrcmp(char* str1, char* str2);
-
+void TestRev();
 #define MAXLINES 5000
 char *lineptrs[MAXLINES];
 
 int main() {
+
+    TestRev();
 
     FILE* file;
     file = fopen("/home/alexey/CLionProjects/Onegin_2/Onegin", "r");
---
+++ reversestrcmp.cpp
@@ -0,0 +1,52 @@
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+
+int RevStrcmp(char* str1, char* str2)
+{
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+
+    int i = 0;
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    while (i < len1 && i < len2)
+    {
+        if (str1[len1 - i - 1] != str2[len2 - i - 1])
+        {
+            return str2[len2 - i - 1] - str1[len1 - i - 1];
+        }
+        i++;
+    }
+    return len2 - len1;
+}
+
+void CheckRev(char *str1, char *str2, int answer)
+{
+    assert(str1 != NULL);
+    assert(str2 != NULL);
+
+    if (answer * RevStrcmp(str1, str2) > 0)
+    {
+        printf("OK\n");
+        return;
+    }
+    if (answer == 0 && RevStrcmp(str1, str2) == 0)
+    {
+        printf("OK\n");
+        return;
+    }
+    printf("FAIL %s\t%s\n", str1, str2);
+}
+
+void TestRev()
+{
+    printf("Tests for RevStrcmp\n");
+    CheckRev("abcd", "dcba", -1);
+    CheckRev("abcd", "edcba", -1);
+    CheckRev("asdf", "asdf", 0);
+    CheckRev("asdfg", "asdfк", 1);
+    CheckRev("f", "k", 1);
+    CheckRev("fk", "kkk", 1);
+}
